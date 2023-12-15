ebc17352d6052d4b62154131163df6cdd2a1f929
FTMFR <fatemefarajzade018@gmail.com>
2023-12-15 12:44:55 +0330

    initial components

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,9 +2,15 @@ import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import { fetchDataFromApi } from "./utils/api";
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfig } from "./store/homeSlice";
 import "./App.css";
 
 function App() {
+  const disPatch = useDispatch();
+  const url = useSelector((state) => state.home);
+  console.log(url);
+
   useEffect(() => {
     apiTesting();
   }, []);
@@ -12,10 +18,16 @@ function App() {
   const apiTesting = () => {
     fetchDataFromApi("/movie/popular").then((res) => {
       console.log(res);
+      disPatch(getApiConfig(res));
     });
   };
 
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      App
+      {/* {url?.total_pages} */}
+    </div>
+  );
 }
 
 export default App;
