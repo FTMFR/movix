567699a6950cdb927bdf020648671b0083127d89
FTMFR <fatemefarajzade018@gmail.com>
2023-12-15 15:30:23 +0330

    fix bug and initial pages and components

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,9 +1,15 @@
 import { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import { fetchDataFromApi } from "./utils/api";
 import { useSelector, useDispatch } from "react-redux";
+import { Route, Routes } from "react-router-dom";
+import { fetchDataFromApi } from "./utils/api";
 import { getApiConfig } from "./store/homeSlice";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import pageNotFound from "./pages/404/pageNotFound";
+import Details from "./pages/details/Details";
+import Explore from "./pages/explore/Explore";
+import SearchResults from "./pages/searchResult/SearchResults";
 import "./App.css";
 
 function App() {
@@ -23,10 +29,9 @@ function App() {
   };
 
   return (
-    <div className="App">
-      App
-      {/* {url?.total_pages} */}
-    </div>
+    <Routes>
+      <Route path="/" element />
+    </Routes>
   );
 }
 
