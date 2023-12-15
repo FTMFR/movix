71e20e81af9a94020f7ebf9d9b4bfad889b349c8
FTMFR <fatemefarajzade018@gmail.com>
2023-12-15 18:17:24 +0330

    create useFetch hook

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,12 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { fetchDataFromApi } from "./utils/api";
 import { getApiConfig } from "./store/homeSlice";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/home/Home";
-import pageNotFound from "./pages/404/pageNotFound";
+import PageNotFound from "./pages/404/pageNotFound";
 import Details from "./pages/details/Details";
 import Explore from "./pages/explore/Explore";
 import SearchResults from "./pages/searchResult/SearchResults";
@@ -17,21 +17,30 @@ function App() {
   const url = useSelector((state) => state.home);
   console.log(url);
 
+  
   useEffect(() => {
+    const apiTesting = () => {
+      fetchDataFromApi("/movie/popular").then((res) => {
+        console.log(res);
+        disPatch(getApiConfig(res));
+      });
+    };
     apiTesting();
-  }, []);
-
-  const apiTesting = () => {
-    fetchDataFromApi("/movie/popular").then((res) => {
-      console.log(res);
-      disPatch(getApiConfig(res));
-    });
-  };
+  }, [disPatch]);
+  
 
   return (
-    <Routes>
-      <Route path="/" element />
-    </Routes>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:mediaType/:id" element={<Details />} />
+        <Route path="/search/:query" element={<SearchResults />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
--- vite-project/src/pages/404/pageNotFound.jsx
+++ vite-project/src/pages/404/pageNotFound.jsx
@@ -1,8 +1,7 @@
-import React from "react";
 import "./style.scss";
 
-const pageNotFound = () => {
+const PageNotFound = () => {
   return <div>pageNotFound</div>;
 };
 
-export default pageNotFound;
+export default PageNotFound;
