2480876e4a2d851ef922a3329d3488e7a5081026
FTMFR <fatemefarajzade018@gmail.com>
2023-12-15 12:18:31 +0330

    initial redux

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,9 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import { fetchDataFromApi } from "./utils/api";
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    apiTesting();
+  }, []);
+
+  const apiTesting = () => {
+    fetchDataFromApi("/movie/popular").then((res) => {
+      console.log(res);
+    });
+  };
 
   return <div className="App">App</div>;
 }
