6276cea45e7f3f08c1049b1358f0443e24567ad6
FTMFR <fatemefarajzade018@gmail.com>
2023-12-17 23:36:39 +0330

    add carousel component

--- vite-project/src/components/carousel/Carousel.jsx
+++ vite-project/src/components/carousel/Carousel.jsx
@@ -10,6 +10,7 @@ import ContentWrapper from "../contentWrapper/ContentWrapper";
 import Img from "../lazyLoadingImage/img";
 import PosterFallback from "../../assets/no-poster.png";
 import CircleRating from "../circleRating/CircleRating";
+import Genres from "../genres/Genres";
 import "./style.scss";
 
 const Carousel = ({ data, loading }) => {
@@ -18,7 +19,18 @@ const Carousel = ({ data, loading }) => {
   const navigate = useNavigate();
 
   const navigation = (dir) => {
-    console.log(dir);
+    const container = carouselContainer.current;
+    console.log(container);
+
+    const scrollAmount =
+      dir === "left"
+        ? container.scrollLeft - (container.offsetWidth + 20)
+        : container.scrollLeft + (container.offsetWidth + 20);
+
+    container.scrollTo({
+      left: scrollAmount,
+      behavior: "smooth",
+    });
   };
 
   const skItem = () => {
@@ -45,21 +57,21 @@ const Carousel = ({ data, loading }) => {
           onClick={() => navigation("right")}
         />
         {!loading ? (
-          <div className="carouselItems">
+          <div className="carouselItems" ref={carouselContainer}>
             {data?.map((item) => {
               const posterUrl = item.poster_path
                 ? url.poster + item.poster_path
                 : PosterFallback;
-              console.log(data);
               return (
-                <div className="carouselItem" key={item.id}>
+                <div
+                  className="carouselItem"
+                  key={item.id}
+                  onClick={() => navigate(`/${item.media_type}/${item.id}`)}
+                >
                   <div className="posterBlock">
                     <Img src={posterUrl} />
-                    <CircleRating 
-                    rating={item.vote_average.toFixed(
-                        1
-                    )}
-                    />
+                    <CircleRating rating={item.vote_average.toFixed(1)} />
+                    <Genres data={item.genre_ids.slice(0, 2)} />
                   </div>
                   <div className="textBlock">
                     <span className="title">{item.title || item.name}</span>
