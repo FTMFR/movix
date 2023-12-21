1786a00dc06f7d66375ead993bd178d298fc193f
FTMFR <fatemefarajzade018@gmail.com>
2023-12-21 21:53:06 +0330

    add search result component

--- vite-project/src/pages/searchResult/SearchResults.jsx
+++ vite-project/src/pages/searchResult/SearchResults.jsx
@@ -1,7 +1,56 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { fetchDataFromApi } from "../../utils/api";
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
+import MovieCard from "../../components/movieCard/MovieCard";
+import Spinner from "../../components/spinner/Spinner";
+import noResults from "../../assets/no-results.png";
 import "./style.scss";
 
 const SearchResults = () => {
-  return <div>SearchResults</div>;
+  const [data, setData] = useState(null);
+  const [pageNum, setPageNum] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const { query } = useParams();
+
+  const fetchInitialData = () => {
+    setLoading(true);
+    fetchDataFromApi(`/searcg/multi?query=${query}&page=${pageNum}`).then(
+      (res) => {
+        setData(res);
+        setPageNum((prev) => prev + 1);
+        setLoading(false);
+        console.log(res);
+      }
+    );
+  };
+
+  const fetchNextPageData = () => {
+    fetchDataFromApi(`/search/multi?query=${query}&page=${pageNum}`).then(
+      (res) => {
+        if (data?.results) {
+          setData({
+            ...data,
+            results: [...data?.results, ...res.results],
+          });
+        } else {
+          setData(res);
+        }
+        setPageNum((prev) => prev + 1);
+      }
+    );
+  };
+
+  useEffect(() => {
+    fetchInitialData();
+  }, [query]);
+
+  return <div className="searchResultsPage">
+    {!loading && (
+      
+    )}
+  </div>;
 };
 
 export default SearchResults;
